a7c1a2da0f49c55397982fad2b5128df176b43bd
Mark Guinn <mark.guinn@inrix.com>
2016-10-11 23:12:57 -0700

    data reorg

--- appView.js
+++ appView.js
@@ -35,34 +35,34 @@ define(function (require) {
                     wrapAroundSelect: false,
                     data: [{
                         icon: 'beer',
-                        text: ['Beer!', '', 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.'],
+                        text: ['Beer!', 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.'],
                     }, {
                         icon: 'book',
-                        text: ['Books', '', 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.'],
+                        text: ['Books', 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.'],
                     }, {
                         icon: 'bell',
-                        text: ['Bells', '', 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.']
+                        text: ['Bells', 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.']
                     }, {
                         icon: 'coffee',
-                        text: ['Coffee', '', 'Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.']
+                        text: ['Coffee', 'Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.']
                     }, {
                         icon: 'cutlery',
-                        text: ['Cutlery', '', 'Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.']
+                        text: ['Cutlery', 'Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.']
                     }, {
                         icon: 'beer',
-                        text: ['Beer', '', 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.'],
+                        text: ['Beer', 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.'],
                     }, {
                         icon: 'book',
-                        text: ['Books', '', 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.'],
+                        text: ['Books', 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.'],
                     }, {
                         icon: 'bell',
-                        text: ['Bells', '', 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.']
+                        text: ['Bells', 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.']
                     }, {
                         icon: 'coffee',
-                        text: ['Coffee', '', 'Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.']
+                        text: ['Coffee', 'Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.']
                     }, {
                         icon: 'cutlery',
-                        text: ['Cutlery', '', 'Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.']
+                        text: ['Cutlery', 'Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.']
                     }]
                 }, false);
 
@@ -129,6 +129,22 @@ define(function (require) {
                     },
                     click() {
                         list.set('scroll', 'page');
+                    }
+                }));
+                p.addChild(new Button({
+                    model: {
+                        text: 'Bar'
+                    },
+                    click() {
+                        list.set('scrollControl', 'scrollbar');
+                    }
+                }));
+                p.addChild(new Button({
+                    model: {
+                        text: 'Dots'
+                    },
+                    click() {
+                        list.set('scrollControl', 'dots');
                     }
                 }));
                 p.render(this.getView());
